f88d026dd90a0c61f0a2ccb1ec69ca7829a56e24
Jorge <jorgenavadelapena@gmail.com>
2023-05-03 19:08:44 -0600

    Updated server

--- server/index.js
+++ server/index.js
@@ -3,14 +3,12 @@ require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const body_parser = require('body-parser');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
 const { verifyToken } = require('./middlewares');
 
 const startServer = async () => {
   const PORT = process.env.PORT || 3000;
   const app = express().use(body_parser.json());
-  const apiV1Router = express.Router();
+  const router = express.Router();
 
   const CONFIG = {};
 
@@ -19,41 +17,24 @@ const startServer = async () => {
     optionsSuccessStatus: 200,
   };
 
-  app.use('/api/v1', apiV1Router);
-  apiV1Router.use(cors(CORS_OPTIONS));
-  apiV1Router.use(verifyToken);
-
-/*   apiV1Router.post('/authenticate', (req, res) => {
-    const REACT_CLIENT_USER = process.env.REACT_CLIENT_API_USER;
-    const REACT_CLIENT_PASSWORD = process.env.REACT_CLIENT_API_PASSWORD;
-    const API_SECRET = process.env.ACCESS_TOKEN_SECRET;
-    const { username, password } = req.body;
-
-    const user = REACT_CLIENT_USER === username;
-
-    if (!user) {
-      return res.status(401).json({ message: 'Authentication failed' });
-    }
-
-    bcrypt.compare(password, REACT_CLIENT_PASSWORD, (err, result) => {
-      if (err || !result) {
-        return res.status(401).json({ message: 'Authentication failed' });
-      }
-      const token = jwt.sign({ username }, API_SECRET, {
-        expiresIn: '1y',
-      });
-      res.json({ token });
-    });
-  }); */
+  app.use('/', (req, res) => {
+    res.json({
+      message: 'Hello ;D'
+    })
+  });
+  
+  app.use('/api/v1', router);
+  router.use(cors(CORS_OPTIONS));
+  router.use(verifyToken);
 
   await require('catalog-express-routes').initByConfiguration(
     CONFIG,
-    apiV1Router
+    router
   );
 
   await require('users-express-routes').initByConfiguration(
     CONFIG,
-    apiV1Router
+    router
   );
 
   app.listen(PORT, () =>
