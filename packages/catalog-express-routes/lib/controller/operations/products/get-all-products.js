474a4623990889a538c836c3d74b6b0995ed06ed
Jorge <jorgenavadelapena@gmail.com>
2023-05-12 15:32:08 -0600

    Small fixes

--- packages/catalog-express-routes/lib/controller/operations/products/get-all-products.js
+++ packages/catalog-express-routes/lib/controller/operations/products/get-all-products.js
@@ -5,7 +5,7 @@ const {CATALOG_OPERATIONS} = require('airtable-client-provider');
 const getAllProducts = async (req, res) => {
   try {
     const OPERATION_RESULT = await CATALOG_OPERATIONS.getAllProducts();
-    if (Object.keys(OPERATION_RESULT).length) {
+    if (Object.keys(OPERATION_RESULT).length || Array.isArray(OPERATION_RESULT)) {
       res.status(200).send(OPERATION_RESULT);
     } else {
       res.status(500).send({error: OPERATION_RESULT.message});
